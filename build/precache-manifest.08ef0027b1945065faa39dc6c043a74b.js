21154162548005809aa402d922b0c7d5d1fbddfe
Unknown <cool24794@gmail.com>
2020-02-11 15:42:38 -0500

    proyecto

---
+++ build/precache-manifest.08ef0027b1945065faa39dc6c043a74b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5309724ef8de2e4d077c149d676e269f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe799964f92eaa3dc5cc",
+    "url": "/static/css/main.6a4ca348.chunk.css"
+  },
+  {
+    "revision": "fc76a2a32d53980dc8f7",
+    "url": "/static/js/2.1a68e4c9.chunk.js"
+  },
+  {
+    "revision": "fe799964f92eaa3dc5cc",
+    "url": "/static/js/main.a73b2570.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/static/media/logo.427a4adc.svg"
+  }
+]);
